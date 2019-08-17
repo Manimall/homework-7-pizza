8c2172c3ee8fb5bff7cfb68b62b3f416173c066a
Manimall <gorpunov94@mail.ru>
2019-08-17 21:59:01 +0300

    1 test not passed test

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -7,12 +7,65 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Обратите внимание на `orders.test.js`.
 // Он поможет понять, какие значения должен возвращать редьюсер.
 
-export default (state = [], action) => {
+const OrdersPositions = {
+  clients: { next: 'conveyor_1', prev: null },
+  conveyor_1: { next: 'conveyor_2', prev: null },
+  conveyor_2: { next: 'conveyor_3', prev: 'conveyor_1' },
+  conveyor_3: { next: 'conveyor_4', prev: 'conveyor_2' },
+  conveyor_4: { next: 'finish', prev: 'conveyor_3' }
+};
+
+const moveOrders = (arr, posToCompare, method, action) => {
+  return arr.map(el => {
+    if (el.id === action.payload) {
+      return (el.position === posToCompare ||
+              (el.position === `conveyor_4` && el.ingredients.length === 0)) ?
+        el :
+        {...el, position: OrdersPositions[el.position][method]};
+    } else {
+      return el;
+    }
+  });
+};
+
+const PizzaReducer = (state = [], action) => {
   switch (action.type) {
+    case(CREATE_NEW_ORDER): {
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          ingredients: [],
+          position: 'clients',
+          recipe: action.payload.recipe
+        }
+      ];
+    }
+    case(MOVE_ORDER_NEXT): {
+      return moveOrders(state, `finish`, `next`, action);
+    }
+    case(MOVE_ORDER_BACK): {
+      return moveOrders(state, `clients`, `prev`, action);
+    }
+    case(ADD_INGREDIENT): {
+      const newItem = action.payload.ingredient;
+
+      const newState = state.map(el => {
+        const ingredients = el.ingredients;
+        return el.recipe.includes(newItem) ?
+          {...el, ingredients: [...ingredients, newItem]} :
+          el;
+      });
+
+      return newState;
+    }
     default:
       return state;
   }
 };
 
+
 export const getOrdersFor = (state, position) =>
   state.orders.filter(order => order.position === position);
+
+export default PizzaReducer;
