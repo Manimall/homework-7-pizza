f793e300a58143188e805dd2c9c86272d3bde199
Manimall <gorpunov94@mail.ru>
2019-08-18 17:08:36 +0300

    changes acc to reviewer

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -9,17 +9,24 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 
 const OrdersPositions = {
   clients: { next: 'conveyor_1', prev: null },
-  conveyor_1: { next: 'conveyor_2', prev: 'conveyor_1' },
+  conveyor_1: { next: 'conveyor_2', prev: 'clients' },
   conveyor_2: { next: 'conveyor_3', prev: 'conveyor_1' },
   conveyor_3: { next: 'conveyor_4', prev: 'conveyor_2' },
   conveyor_4: { next: 'finish', prev: 'conveyor_3' }
 };
 
+const OrdersPositionsKeys = Object.keys(OrdersPositions);
+
+const lastOrdersPosition = OrdersPositionsKeys[OrdersPositionsKeys.length-1];
+const firstOrdersPosition = OrdersPositionsKeys[0];
+const firstConveyorPosition = OrdersPositionsKeys[1];
+
+
 const moveOrders = (arr, posToCompare, method, action) => {
   return arr.map(el => {
     if (el.id === action.payload) {
-      return (el.position === posToCompare ||
-              (el.position === Object.keys(OrdersPositions)[Object.keys(OrdersPositions).length-1] && el.ingredients.length === 0)) ?
+      return ((el.position === posToCompare && el.ingredients.length < el.recipe.length) ||
+              (el.position === firstConveyorPosition && method === `prev`)) ?
         el :
         {...el, position: OrdersPositions[el.position][method]};
     } else {
@@ -36,16 +43,16 @@ const PizzaReducer = (state = [], action) => {
         {
           id: action.payload.id,
           ingredients: [],
-          position: 'clients',
+          position: `clients`,
           recipe: action.payload.recipe
         }
       ];
     }
     case(MOVE_ORDER_NEXT): {
-      return moveOrders(state, `finish`, `next`, action);
+      return moveOrders(state, lastOrdersPosition, `next`, action);
     }
     case(MOVE_ORDER_BACK): {
-      return moveOrders(state, `clients`, `prev`, action);
+      return moveOrders(state, firstOrdersPosition, `prev`, action);
     }
     case(ADD_INGREDIENT): {
       const newItem = action.payload.ingredient;
